5208c5e9fb4264e9abf34fe32f4267fc55252cb1
Oleksandr Masniuk <nephestdev@gmail.com>
2026-09-01 16:57:17 +0000

    replace posix commands with modern bash evaluation

--- container/app/entrypoint.sh
+++ container/app/entrypoint.sh
@@ -28,16 +28,16 @@ if [[ "$SSH_SERVER_ENABLED" == "true" ]]; then
     done
 fi
 
-if [ -d "$GITCONFIG_D" ]; then
+if [[ -d "$GITCONFIG_D" ]]; then
     for file in $GITCONFIG_D/*; do
-        if [ -f "$file" ]; then
+        if [[ -f "$file" ]]; then
             echo "Gitconfig $file"
             echo -e "\n[include]\n    path = $file" >> "$GITCONFIG"
         fi
     done
 fi
 
-if [ -f "$PAT_FILE" ]; then
+if [[ -f "$PAT_FILE" ]]; then
     echo "Github PAT $PAT_FILE"
     gh auth login --with-token < "$PAT_FILE"
     gh auth setup-git
