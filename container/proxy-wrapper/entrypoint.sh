230e9ea183a594bf808dd59f2a024b99aff9fabf
Oleksandr Masniuk <nephestdev@gmail.com>
2026-09-02 08:30:45 +0000

    use ss client config in ss-nat

--- container/proxy-wrapper/entrypoint.sh
+++ container/proxy-wrapper/entrypoint.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 SS_CLIENT_CONFIG_FILE="$HOME/.config/shadowsocks/client.json"
 
 if [[ -f "$SS_CLIENT_CONFIG_FILE" ]]; then
-    ss-nat -s "$SS_SERVER_ADDRESS" -l 1080 -o
+    ss-nat \
+        -s $(jq -r '.server' "$SS_CLIENT_CONFIG_FILE") \
+        -l $(jq -r '.local_port' "$SS_CLIENT_CONFIG_FILE") \
+        -o
 else
     echo "$SS_CLIENT_CONFIG_FILE not found"
     exit 1
